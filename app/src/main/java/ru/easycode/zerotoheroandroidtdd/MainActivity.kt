126bee42623f03fe13d7cce5985e00536ec20999
NumpyGlumpy <khaypov03@mail.ru>
2023-11-19 19:06:48 +0300

    - task/012 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,39 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+    private var uiState: UiState = UiState.Base("0")
+    private val count: Count = Count.Base(2, 12)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.countTextView)
+        button = findViewById(R.id.incrementButton)
+        button.setOnClickListener {
+            uiState = count.increment(textView.text.toString())
+            uiState.apply(textView, button)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable("key", uiState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            uiState = savedInstanceState.getSerializable("key", UiState::class.java) as UiState
+        }
+        uiState.apply(textView, button)
     }
 }
