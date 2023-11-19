a0ec023020f1d661a12a36caacdb8e4c91d1881a
NumpyGlumpy <khaypov03@mail.ru>
2023-11-19 21:14:18 +0300

    - task/013 solved

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/Count.kt
@@ -0,0 +1,48 @@
+package ru.easycode.zerotoheroandroidtdd
+
+interface Count {
+
+    fun initial(number: String): UiState
+
+    fun increment(number: String): UiState
+
+    fun decrement(number: String): UiState
+
+    class Base(private val step: Int, private val max: Int, private val min: Int) : Count {
+        init {
+            if (step < 1)
+                throw IllegalStateException("step should be positive, but was $step")
+            if (max < 1)
+                throw IllegalStateException("max should be positive, but was $max")
+            if (max < step) {
+                throw IllegalStateException("max should be more than step")
+            }
+            if (max < min)
+                throw IllegalStateException("max should be more than min")
+        }
+
+        override fun initial(number: String): UiState {
+            return if (number.toInt() == min)
+                UiState.Min(number)
+            else if (number.toInt() == max)
+                UiState.Max(number)
+            else UiState.Base(number)
+        }
+
+        override fun increment(number: String): UiState {
+            val result = (step + number.toInt())
+            return if (result + step <= max) {
+                UiState.Base(result.toString())
+            } else
+                UiState.Max(result.toString())
+        }
+
+        override fun decrement(number: String): UiState {
+            val result = (number.toInt() - step)
+            return if (result - step >= min) {
+                UiState.Base(result.toString())
+            } else
+                UiState.Min(result.toString())
+        }
+    }
+}
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,49 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
+    private var count: Count = Count.Base(2, 4, 0)
+    private lateinit var incrementButton: Button
+    private lateinit var decrementButton: Button
+    private lateinit var state: UiState
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.countTextView)
+        incrementButton = findViewById(R.id.incrementButton)
+        decrementButton = findViewById(R.id.decrementButton)
+        incrementButton.setOnClickListener {
+            state = count.increment(textView.text.toString())
+            state.apply(textView, incrementButton, decrementButton)
+        }
+        decrementButton.setOnClickListener {
+            state = count.decrement(textView.text.toString())
+            state.apply(textView, incrementButton, decrementButton)
+        }
+
+        if (savedInstanceState == null) {
+            state = count.initial(textView.text.toString())
+            state.apply(textView, incrementButton, decrementButton)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable("key", state)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        state = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            savedInstanceState.getSerializable("key", UiState::class.java) as UiState
+        } else
+            savedInstanceState.getSerializable("key") as UiState
+        state.apply(textView, incrementButton, decrementButton)
     }
 }
