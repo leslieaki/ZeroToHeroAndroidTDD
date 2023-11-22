285a150de16469f2c994b39b2d9affba50b57bbe
NumpyGlumpy <khaypov03@mail.ru>
2023-11-22 03:41:58 +0300

    - task/017 added necessary files

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,26 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewModel = (application as App).viewModel
+        val textView = findViewById<TextView>(R.id.titleTextView)
+        val button = findViewById<Button>(R.id.actionButton)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
+        button.setOnClickListener {
+            viewModel.load()
+        }
+
+        viewModel.liveData().observe(this) {
+            it.apply(textView, button, progressBar)
+        }
     }
 }
---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainViewModel.kt
@@ -0,0 +1,30 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import androidx.lifecycle.LiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+
+class MainViewModel(
+    private val liveDataWrapper: LiveDataWrapper,
+    private val repository: Repository
+) : ProvideViewModel {
+    private val viewModelScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
+
+    fun load() {
+        viewModelScope.launch {
+            repository.load()
+        }
+    }
+
+    fun save(bundleWrapper: BundleWrapper.Save) {
+        liveDataWrapper.save(bundleWrapper)
+    }
+
+    fun restore(bundleWrapper: BundleWrapper.Restore) {
+        liveDataWrapper.update(bundleWrapper.restore())
+    }
+
+    override fun liveData(): LiveData<UiState> = liveDataWrapper.liveData()
+}
