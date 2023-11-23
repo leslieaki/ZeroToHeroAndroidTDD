fbd25063f1a764b5b4f7eb09988719d87a7523b3
NumpyGlumpy <khaypov03@mail.ru>
2023-11-23 23:24:49 +0300

    - task/019 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/App.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/App.kt
@@ -10,7 +10,7 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         val retrofit = Retrofit.Builder()
-            .baseUrl(URL)
+            .baseUrl("https://www.google.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         val service = retrofit.create(SimpleService::class.java)
