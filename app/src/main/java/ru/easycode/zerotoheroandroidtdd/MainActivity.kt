afcae7d513ebbc48f4c5dfaef735831e49642626
NumpyGlumpy <khaypov03@mail.ru>
2023-11-24 20:39:44 +0300

    - task/021 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,37 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.titleTextView)
+        val button = findViewById<Button>(R.id.actionButton)
+        val inputEditText = findViewById<TextInputEditText>(R.id.inputEditText)
+
+        inputEditText.addTextChangedListener {
+            button.isEnabled = inputEditText.text.toString().length >= 3
+        }
+
+        button.setOnClickListener {
+            textView.text = inputEditText.text
+            inputEditText.setText("")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("key", textView.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        textView.text = savedInstanceState.getString("key")
     }
 }
