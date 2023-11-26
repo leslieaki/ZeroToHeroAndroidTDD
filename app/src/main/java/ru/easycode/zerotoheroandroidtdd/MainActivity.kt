0684f1969100cab8c1b5759883641120ce6d57a2
NumpyGlumpy <khaypov03@mail.ru>
2023-11-26 11:16:34 +0300

    - task/023 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -7,10 +7,37 @@ import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var list = ArrayList<String>()
+    private val adapter = RecyclerViewAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.recyclerView.adapter = adapter
+        binding.actionButton.setOnClickListener {
+            val text = binding.inputEditText.text.toString()
+            adapter.add(text)
+            list.add(text)
+            binding.inputEditText.text?.clear()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putStringArrayList(KEY, list)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        list = savedInstanceState.getStringArrayList(KEY)!!
+        list.forEach {
+            adapter.add(it)
+        }
+    }
+
+    companion object {
+        private const val KEY = "key"
     }
 }
---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/RecyclerViewAdapter.kt
@@ -0,0 +1,33 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import ru.easycode.zerotoheroandroidtdd.databinding.ListElementBinding
+
+class RecyclerViewAdapter : Adapter<ViewHolder>() {
+
+    private val itemsList = ArrayList<String>()
+
+    fun add(source: String) {
+        itemsList.add(source)
+        notifyItemInserted(itemsList.size - 1)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(ListElementBinding.inflate(LayoutInflater.from(parent.context)))
+
+    override fun getItemCount(): Int = itemsList.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(itemsList[position])
+    }
+}
+
+class ViewHolder(private val binding: ListElementBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(source: String) {
+        binding.elementTextView.text = source
+    }
+}
